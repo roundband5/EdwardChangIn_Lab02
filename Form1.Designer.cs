2b78a3d970f2c7b026914664489e86918269847e
roundband5 <30790329+roundband5@users.noreply.github.com>
2018-10-01 17:40:41 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace EdwardChangIn_Lab02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCarType = new System.Windows.Forms.ComboBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtCarMake = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblCarMake = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cboCarType
+            // 
+            this.cboCarType.BackColor = System.Drawing.Color.Gray;
+            this.cboCarType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCarType.FormattingEnabled = true;
+            this.cboCarType.Items.AddRange(new object[] {
+            "Sedan",
+            "SUV",
+            "Truck",
+            "Bus"});
+            this.cboCarType.Location = new System.Drawing.Point(190, 129);
+            this.cboCarType.Name = "cboCarType";
+            this.cboCarType.Size = new System.Drawing.Size(185, 21);
+            this.cboCarType.TabIndex = 0;
+            this.cboCarType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(45, 307);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(157, 307);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(273, 307);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtCarMake
+            // 
+            this.txtCarMake.Location = new System.Drawing.Point(190, 61);
+            this.txtCarMake.Name = "txtCarMake";
+            this.txtCarMake.Size = new System.Drawing.Size(185, 20);
+            this.txtCarMake.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(190, 98);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(185, 20);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // lblCarMake
+            // 
+            this.lblCarMake.AutoSize = true;
+            this.lblCarMake.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarMake.Location = new System.Drawing.Point(51, 64);
+            this.lblCarMake.Name = "lblCarMake";
+            this.lblCarMake.Size = new System.Drawing.Size(92, 20);
+            this.lblCarMake.TabIndex = 6;
+            this.lblCarMake.Text = "Car\'s Make:";
+            this.lblCarMake.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(48, 98);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(52, 20);
+            this.lblPrice.TabIndex = 7;
+            this.lblPrice.Text = " Price:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(51, 132);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(47, 20);
+            this.lblType.TabIndex = 8;
+            this.lblType.Text = "Type:";
+            this.lblType.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(54, 174);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(308, 101);
+            this.txtOutput.TabIndex = 10;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 352);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblCarMake);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtCarMake);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.cboCarType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCarType;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtCarMake;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblCarMake;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EdwardChangIn_Lab02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            string carMake = txtCarMake.Text;
+            double price = double.Parse(txtPrice.Text);
+            string carType = cboCarType.Text;
+            txtOutput.Text = "The car's make is " + carMake+"." + "\r\n Price is: $"+ price+ "\r\n The Car's Type is a "+ carType+".";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCarMake.Text = "";
+            cboCarType.SelectedIndex = -1;
+            txtOutput.Text = "";
+            txtPrice.Text = "";
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
